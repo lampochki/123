c7b38f877f30040f802aefe63241305fca616c92
lampochki <shtostya@yandex.ru>
2018-05-28 11:22:46 +0300

    aver

--- test258RR.py
+++ test258RR.py
@@ -1,90 +1,29 @@
-import struct
-import os
 import numpy as np
 import numpy.fft as fft
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import struct
+import os
 import fnmatch
 
-np.set_printoptions(threshold=5)
 
-
-
-pp='RR'
-bn=258
 matches=[]
-for path, dirs, files in os.walk(os.getcwd()):
-        for result in [os.path.abspath(os.path.join(path, filename)) for
-                filename in files if fnmatch.fnmatch(filename,'*.b0000')]:
-            matches.append(result)
+for root, dirs, files in os.walk(os.getcwd()):
+    for file in files:
+        if file.endswith('.npy'):
+            print file
+            matches.append(file)
 print matches
 to=len(matches)
-print to
-for fi in range(1,8):
-	for i in range (to):
-		print i
-		fn=str(matches[i])
-		#fn=str('/home/difx/06/RUP009_20130218_18_CRAB/sched-key4_test/s000'+f+str(i))
-		size=os.path.getsize(fn)
-		current_head=0
-		head_count=0
-		accum_periods=0
-		x=0
-		all_periods=0
-		with open(fn, 'rb') as sw:
-			sync=int(struct.unpack('i', sw.read(4))[0])
-			sync_search=0 
-			sync_distance=0
-			while sync_search!=sync:
-				sync_distance+=1
-				sw.seek(sync_distance)
-				sync_search=int(struct.unpack('i', sw.read(4))[0])
-			print 'distance between syncwords', sync_distance
-			spec_chan=(sync_distance-74)/8
-			while head_count<size/sync_distance:
-				sw.seek(sync_distance*current_head)
-				sync=str(sw.read(4))
-				head_version,=struct.unpack('i', sw.read(4))
-				baseline_number,=struct.unpack('i', sw.read(4))
-				MJD,=struct.unpack('i', sw.read(4))
-				seconds,=struct.unpack('d', sw.read(8))
-				config_index,=struct.unpack('i', sw.read(4))
-				source_index,=struct.unpack('i', sw.read(4))
-				freq_index,=struct.unpack('i', sw.read(4))
-				polarisation_pair=str(sw.read(2))
-				pulsar_bin,=struct.unpack('i', sw.read(4))
-				data_weight,=struct.unpack('d', sw.read(8))
-				U,=struct.unpack('d', sw.read(8))
-				V,=struct.unpack('d', sw.read(8))
-				W,=struct.unpack('d', sw.read(8))
-				if baseline_number==bn and polarisation_pair==pp and freq_index==fi:
-					accum_periods+=1
-				current_head+=1
-				head_count+=1
-			head_count=0
-			current_head=0
-			data=np.zeros((accum_periods,spec_chan), dtype=np.complex)
-			while head_count<size/sync_distance:
-				sw.seek(sync_distance*current_head)
-				sync=str(sw.read(4))
-				head_version,=struct.unpack('i', sw.read(4))
-				baseline_number,=struct.unpack('i', sw.read(4))
-				MJD,=struct.unpack('i', sw.read(4))
-				seconds,=struct.unpack('d', sw.read(8))
-				config_index,=struct.unpack('i', sw.read(4))
-				source_index,=struct.unpack('i', sw.read(4))
-				freq_index,=struct.unpack('i', sw.read(4))
-				polarisation_pair=str(sw.read(2))
-				pulsar_bin,=struct.unpack('i', sw.read(4))
-				data_weight,=struct.unpack('d', sw.read(8))
-				U,=struct.unpack('d', sw.read(8))
-				V,=struct.unpack('d', sw.read(8))
-				W,=struct.unpack('d', sw.read(8))
-				if baseline_number==bn and polarisation_pair==pp and freq_index==fi:
-					for y in range (spec_chan):
-						data[x,y]= complex(struct.unpack('f', sw.read(4))[0], 
-							struct.unpack('f', sw.read(4))[0])
-					x+=1
-				head_count+=1
-				current_head+=1
-			all_periods+=accum_periods
-		np.save(str(i)+'bn'+str(bn)+'fq'+str(fi),data)
-		print data
+for i in range(to):
+	fn=str(matches[i])
+	corr=np.load(fn)
+	aver=34
+	#print np.shape
+	spec_chan=corr.shape[1]
+	periods=corr.shape[0]
+	aver_data=np.zeros((aver-1,spec_chan),dtype=np.complex)
+	for k in range(periods/aver):
+		aver_data[0:33]+=corr[k*aver:(k+1)*aver-1]
+	np.save('aver'+str(fn),aver_data)
+	print 'done'
